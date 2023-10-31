c7cf78f5bb0f37d6fcd395c27ced6c7a7ac3841f
sri-RagulRagul <144445156+sri-RagulRagul@users.noreply.github.com>
2023-10-31 12:58:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
-
-    def __repr__(self):
-        return f"{self.name}, Roll Number: {self.roll_number}, CGPA: {self.cgpa}"
-
-def sort_students(student_list):
-    return sorted(student_list, key=lambda x: x.cgpa, reverse=True)
-
-# Test the function with different input lists of students
-students_list = [
-    Student("Alice", "A001", 3.8),
-    Student("Bob", "B002", 3.5),
-    Student("Charlie", "C003", 3.9),
-    Student("David", "D004", 3.7)
-]
-
-sorted_students = sort_students(students_list)
-for student in sorted_students:
-    print(student)
+def search(List, n):
   
+    for i in range(len(List)):
+        if List[i] == n:
+            return True
+    return False
+  
+  
+# list which contains both string and numbers.
+List = [1, 2, 'sachin', 4, 'Geeks', 6]
+  
+# Driver Code
+n = 'Geeks'
+  
+if search(List, n):
+    print("Found")
+else:
+    print("Not Found")
